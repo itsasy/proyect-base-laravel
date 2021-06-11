a539ccd9a1bbd441ab3ea548c7470584bb72e86c
itsasy <ale.maldo097@gmail.com>
2021-06-10 19:45:48 -0500

    Use Tymon JWT

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => '/v1', 'as' => 'api.'], function () {
+    Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
+        Route::post('login', 'AuthController@login')->name('api.name');
+        Route::post('logout', 'AuthController@logout')->name('api.logout');
+        Route::post('refresh', 'AuthController@refresh')->name('api.refresh');
+        Route::post('me', 'AuthController@me')->name('api.me');
+    });
 });
+
